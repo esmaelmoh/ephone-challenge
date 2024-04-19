00af00c423bddb52900a4efb518de4d061196942
esmaelmoh <esmaelmoh0132@gmail.com>
2024-04-19 14:44:42 +0300

    taiwlind css and config setup finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  darkMode: "class",
+  important: true,
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./safelist.txt"],
   theme: {
+    fontSize: {
+      xs: ["0.5rem", { lineHeight: "normal" }],
+      sm: ["0.6rem", { lineHeight: "normal" }],
+      base: ["1rem", { lineHeight: "normal" }],
+      lg: ["1.25rem", { lineHeight: "normal" }],
+      xl: ["1.5rem", { lineHeight: "normal" }],
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
